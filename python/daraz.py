0902654ffc0cb490d23e21f772f7122a3e3bfa5f
whodanyalahmed <daniahmedkhatri@gmail.com>
2022-06-26 20:20:34 +0500

    daraz.pk completed

--- python/daraz.py
+++ python/daraz.py
@@ -30,7 +30,22 @@ def Chrome(headless=False):
     return driver
 
 
+def minpr(d):
+    if d == 0:
+        return 100000000000000000000
+    else:
+        return 1
+
+
+minprice = minpr(0)
+minname = ""
+mincount = 0
+
+
 def main(keyword):
+    global minprice
+    global minname
+    global mincount
     keyword_url = keyword.replace(' ', '+')
     driver = Chrome(True)
     url = 'https://www.daraz.pk/smartphones/?from=input&q=' + keyword_url
@@ -59,25 +74,42 @@ def main(keyword):
     print(len(price))
     print(len(img_Src))
     price_list = []
+    title_list = []
+    image_list = []
     for i in range(len(title)):
         if keyword.lower() in title[i].text.lower():
 
-        
             print("="*30)
-            
-            print(title[i].text)
 
+            print(title[i].text)
+            title_list.append(title[i].text)
             print(price[i].text)
             int_price = price[i].text.replace('Rs. ', '')
             price_list.append(int_price)
             print(img_Src[i].get_attribute('src'))
+            image_list.append(img_Src[i].get_attribute('src'))
 
     # print(product.text)
     print(price_list)
+    dt = dict(zip(title_list, price_list))
+    print(dt)
+
+    for k, v in dt.items():
+        v = int(v.replace(",", ""))
+
+        if v < minprice:
+            minprice = v
+            minname = k
+            mincount += 1
+
     print("Min. price is : ", min(price_list))
 
+    # return minprice,minname and iamge_link with mincount as index
+
     driver.quit()
+    return {"price": minprice, "name": minname, "src": image_list[mincount]}
 
 
 if __name__ == '__main__':
-    main("Iphone 13 ")
+    d = main("Iphone 13")
+    print(d)
